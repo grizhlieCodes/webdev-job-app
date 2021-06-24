989ee21a9e3ab7dfb83466a3c5227d993612923f
HusJAW <hj25691@gmail.com>
2021-06-24 17:02:02 +0100

    renaming colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        testingColor: 'red'  ,
-        violet700: '#5964E0',
-        violet400: '#939BF4',
-        blue800: '#19202D',
-        blue900: '#121721',
-        white: '#FFFFFF',
-        grey100: '#F4F6F8',
-        grey500: '#9DAEC2',
-        grey700: '#6E8098'
-        
-
+        violet: '#5964E0',
+        'light-violet': '#939BF4',
+        'dark-blue': '#19202D',
+        midnight: '#121721',
+        'light-grey': '#F4F6F8',
+        grey: '#9DAEC2',
+        'dark-grey': '#6E8098'
       }
     },
   },
