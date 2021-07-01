521e372c245025d4128ecf728f4eba4f44137fd5
grizhlieCodes <rafal.potasz@gmail.com>
2021-07-01 15:19:56 +0100

    Added fetch of json

--- scripts/renderHTML.js
+++ scripts/renderHTML.js
@@ -16,3 +16,14 @@ const header = document.querySelector('header')
 const headerInner = header.querySelector('.header-inner-container')
 const toggle = createToggle()
 headerInner.innerHTML += toggle
+
+let jobPostings
+async function fetchData(){
+    var jsonData = await fetch('../assets/jason/data.json')
+    var data = await jsonData.json()
+    jobPostings = data
+    console.log(jobPostings)
+}
+fetchData()
+
+
